b06ecd1a075f470936434f6979af6c939417b0a8
martinolanza03 <lanzamartino3@gmail.com>
2025-04-10 15:10:53 +0200

    feat: create search with genre

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import { useState, useEffect } from 'react';
 
-const film = [
+const films = [
   { title: 'Inception', genre: 'Fantascienza' },
   { title: 'Il Padrino', genre: 'Thriller' },
   { title: 'Titanic', genre: 'Romantico' },
@@ -11,8 +11,54 @@ const film = [
 ];
 
 function App() {
+
+  const [filteredFilm, setFilteredFilm] = useState(films)
+  const [searchGenre, setSearchCategory] = useState('');
+
+  useEffect(() => {
+
+    let updatedFilm = films;
+
+    if (searchGenre !== '') {
+      updatedFilm = updatedFilm.filter(film => film.genre === searchGenre)
+    }
+
+    setFilteredFilm(updatedFilm);
+  }, [searchGenre])
+
   return (
     <>
+      <h1>Movie</h1>
+
+      <hr />
+      <section>
+        <h2>Cerca Film</h2>
+        <label>Cerca per categoria</label>
+        <div>
+
+          <select value={searchGenre} onChange={e => setSearchCategory(e.target.value)}>
+            <option value="">---</option>
+            <option>Fantascienza</option>
+            <option>Thriller</option>
+            <option>Romantico</option>
+            <option>Azione</option>
+          </select>
+
+        </div>
+
+
+      </section>
+
+      <hr />
+
+      <section>
+        <ul>
+          {filteredFilm.map((film, i) => <li key={i}>
+            <h3>{film.title}</h3>
+            <p>Categoria: {film.genre}</p>
+          </li>)}
+        </ul>
+      </section>
 
     </>
   )
