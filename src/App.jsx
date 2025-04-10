ae76602f690702b34aff888970598145550400aa
martinolanza03 <lanzamartino3@gmail.com>
2025-04-10 14:41:40 +0200

    feat: create array and import useState and useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,14 @@
 import './App.css'
+import { useState, useEffect } from 'react';
+
+const film = [
+  { title: 'Inception', genre: 'Fantascienza' },
+  { title: 'Il Padrino', genre: 'Thriller' },
+  { title: 'Titanic', genre: 'Romantico' },
+  { title: 'Batman', genre: 'Azione' },
+  { title: 'Interstellar', genre: 'Fantascienza' },
+  { title: 'Pulp Fiction', genre: 'Thriller' },
+];
 
 function App() {
   return (
